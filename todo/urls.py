4a24d0d3075c82965d6c11d8f09efa4c3bfde869
Yuliia Hladyshkevych <yulia.gladyshkevych@mail.com>
2023-11-27 16:00:47 +0200

    Added task creation, update, and deletion views and templates

--- todo/urls.py
+++ todo/urls.py
@@ -1,12 +1,33 @@
 from django.urls import path
 
-from todo.views import TaskListView, TagListView, TagCreateView, TagUpdateView, TagDeleteView
+from todo.views import TaskListView, TagListView, TagCreateView, TagUpdateView, TagDeleteView, TaskCreateView, \
+    TaskUpdateView, TaskDeleteView, task_complete
 
-urlpatterns =[
+urlpatterns = [
     path(
         "",
         TaskListView.as_view(),
         name="task-list",
+    ),
+    path(
+        "tasks/create/",
+        TaskCreateView.as_view(),
+        name="task-create"
+    ),
+    path(
+        "tasks/<int:pk>/update",
+        TaskUpdateView.as_view(),
+        name="task-update"
+    ),
+    path(
+        "tasks/<int:pk>/delete",
+        TaskDeleteView.as_view(),
+        name="task-delete"
+    ),
+    path(
+        "tasks/<int:pk>/complete",
+        task_complete,
+        name="task-complete"
     ),
     path(
         "tags/",
@@ -15,13 +36,13 @@ urlpatterns =[
     ),
     path(
         "tags/create/",
-         TagCreateView.as_view(),
-         name="tag-create"
+        TagCreateView.as_view(),
+        name="tag-create"
     ),
     path(
         "tags/<int:pk>/update",
-         TagUpdateView.as_view(),
-         name="tag-update"
+        TagUpdateView.as_view(),
+        name="tag-update"
     ),
     path(
         "tags/<int:pk>/delete",
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,5 @@
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy
 from django.views import generic
 
@@ -7,6 +9,31 @@ from todo.models import Tag, Task
 class TaskListView(generic.ListView):
     model = Task
     paginate_by = 5
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("todo:task-list")
+
+
+class TaskUpdateView(generic.UpdateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("todo:task-list")
+
+
+class TaskDeleteView(generic.DeleteView):
+    model = Task
+    success_url = reverse_lazy("todo:task-list")
+
+
+def task_complete(request, pk) -> HttpResponseRedirect:
+    # task = get_object_or_404(Task, id=pk)
+    task = Task.objects.get(id=pk)
+    task.is_done = not task.is_done
+    task.save()
+    return HttpResponseRedirect(reverse_lazy("todo:task-list"))
 
 
 class TagListView(generic.ListView):
